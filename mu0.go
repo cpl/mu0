c819af06f83d3504fda1f0a37559f76f389c7b73
Alexandru-Paul Copil <alexandru.p.copil@pm.me>
2018-12-22 23:16:35 +0200

    Finish flag parsing for main

--- mu0.go
+++ mu0.go
@@ -12,7 +12,7 @@ import (
 const usage = "mu0 <run <source.o> | build <source.s> [source.o]>"
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) < 3 {
 		fmt.Println(usage)
 		os.Exit(1)
 	}
@@ -23,8 +23,17 @@ func main() {
 			compiler.Compile(os.Args[2], os.Args[3])
 		}
 
+		compiler.Compile(os.Args[2], os.Args[2]+".o")
+
 		os.Exit(0)
 	case "run":
+		_vm := vm.New()
+		_vm.LoadFile(os.Args[2])
+		_vm.Run()
+
+		fmt.Println("ACC", _vm.ACC, "PC", _vm.PC, "EXIT CODE", _vm.StopCode)
+
+		os.Exit(0)
 	default:
 		fmt.Println(usage)
 		os.Exit(1)
